44b59f05fd1f3af80d140e5cae83f5c6da25bd61
Katlego Maleka <katlego@bluespine.co.za>
2015-11-20 17:37:16 +0200

    Initial code commit.

---
+++ business/src/main/java/za/co/scmb/nbol/showcase/business/App.java
@@ -0,0 +1,13 @@
+package za.co.scmb.nbol.showcase.business;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ business/workflow/src/main/java/za/co/scmb/nbol/showcase/business/workflow/App.java
@@ -0,0 +1,13 @@
+package za.co.scmb.nbol.showcase.business.workflow;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
